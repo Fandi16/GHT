13a4fe08fdac11082e5a60bb96c2c49e25581e1f
ilhamptrr <ilham.ptrr@gmail.com>
2023-06-14 15:02:25 +0700

    update home

--- src/scripts/views/pages/Detail.js
+++ src/scripts/views/pages/Detail.js
@@ -1,11 +1,22 @@
+import UrlParser from '../../routes/url-parser';
+import data from './DATA.json';
+
 const Detail = {
   async render() {
     return `
-          <h2>Now Detail Page</h2>
+          <div id="detailDestination" class="detailDestination"></div>
         `;
   },
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const destinationId = url.id;
+    const destination = data.wisata.find((r) => r.id === destinationId);
+
+    const detailDestinationElement = document.querySelector('#detailDestination');
+    detailDestinationElement.innerHTML = `
+      <img src="${destination.pictureId}" alt="${destination.name}" title="${destination.name}">
+      <h3>${destination.name}</h3>
+    `;
   },
 };
 
