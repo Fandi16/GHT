68410e96755db57a3025ffe20b531b7e7af6492d
Fandi 16 <fanditok16@gmail.com>
2023-06-14 15:25:10 +0700

    update

--- src/scripts/views/pages/Explore.js
+++ src/scripts/views/pages/Explore.js
@@ -1,11 +1,37 @@
+import data from './DATA.json';
+
 const explore = {
   async render() {
     return `
-          <h2>Now Explore Page</h2>
+          <div class="content-wisata">
+            <div class="list" id="tes"></div>
+        </div>
         `;
   },
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const listElement = document.querySelector('#tes');
+
+    data.wisata.forEach((destination) => {
+      const listItem = `
+        <div class="kategori-wisata">
+            <div class="card">
+                <div class="face front">
+                    <img src="${destination.pictureId}" alt="${destination.name}" title="${destination.name}">
+                    <h3>${destination.name}</h3>
+                </div>
+                <div class="face back">
+                    <h3>${destination.name}</h3>
+                    <p>${destination.description}</p>
+                    <div class="link">
+                        <a href="/#/detail/${destination.id}">Details</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      `;
+
+      listElement.innerHTML += listItem;
+    });
   },
 };
 
