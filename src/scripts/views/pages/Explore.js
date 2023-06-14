b499260420415dee14fa97faa42e4f9ac7c20ed0
ilhamptrr <ilham.ptrr@gmail.com>
2023-06-14 16:03:00 +0700

    add new element

--- src/scripts/views/pages/Explore.js
+++ src/scripts/views/pages/Explore.js
@@ -4,6 +4,7 @@ const explore = {
   async render() {
     return `
         <div class="content-wisata">
+            <h1>Which Destination do you want to go to?</h1>
             <div class="list" id="tes"></div>
         </div>
     `;
