d9986b82cd67e895e4b4c099ef7e43207ad644c0
mon <queenarunia@gmail.com>
2023-06-16 15:56:57 +0700

    edit by monica

--- src/scripts/views/pages/AboutUs.js
+++ src/scripts/views/pages/AboutUs.js
@@ -1,7 +1,90 @@
 const AboutUs = {
   async render() {
     return `
-          <h2>isi abut us</h2>
+      <div class="about-us">
+            <div class="inner-we-do">
+              <h1>What we do</h1>
+              <p>
+                Website Green Hunter Tour(GHT) dibuat untuk membantu wisatawan dalam mencari informasi wisata yang ada di Provinsi Nusa Tenggara Timur, dan juga sebagai ajang promosi daerah wisata Provinsi Nusa Tenggara Timur ke seluruh masyarakat Indonesia.
+                Selain itu, diharapkah dengan adanya website ini dapat membantu perekonomian para pengusaha setempat dengan meningkatnya jumlah wisatawan.
+              </p>
+            </div>
+
+        <section>
+          
+          <div class="row">
+            <h1>Our Team</h1>
+          </div> 
+          
+          <div class="row">
+          
+            <!--  Column 1 -->
+            <div class="column">
+              <div class="card2">
+                <div class="img-container">
+                  <img src="../image/member1.jpg">
+                </div>
+                <h3> Ilham Maulana Putra</h3>
+                <p>Web Developer</p>
+                <div class="icons">
+                  <a href="#" style="color: #3b5999;"><i class="fa-brands fa-facebook"></i></a>
+                  <a href="#" style="color: #55acee;"><i class="fa-brands fa-twitter"></i></a>
+                  <a href="https://www.instagram.com/ilhamptrr/" style="color: #e6683c;"><i class="fa-brands fa-instagram"></i></a>
+                  <a href="https://www.linkedin.com/in/ilhamptrr/" style="color: #012165;"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+              </div>
+            </div>
+
+            <div class="column">
+              <div class="card2">
+                <div class="img-container">
+                  <img src="../image/member2.jpg">
+                </div>
+                <h3>Khoir Afandi</h3>
+                <p>Web Developer</p>
+                <div class="icons">
+                  <a href="#" style="color: #3b5999;"><i class="fa-brands fa-facebook"></i></a>
+                  <a href="#" style="color: #55acee;"><i class="fa-brands fa-twitter"></i></a>
+                  <a href="https://instagram.com/fandi.16_?igshid=ZDc4ODBmNjlmNQ==" style="color: #e6683c;"><i class="fa-brands fa-instagram"></i></a>
+                  <a href="https://www.linkedin.com/in/khoir-afandi-780433204" style="color: #012165;"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+              </div>
+            </div>
+
+            <div class="column">
+              <div class="card2">
+                <div class="img-container">
+                  <img src="../image/member3.jpg">
+                </div>
+                <h3>Monica Audry</h3>
+                <p>Web Developer</p>
+                <div class="icons">
+                  <a href="#" style="color: #3b5999;"><i class="fa-brands fa-facebook"></i></a>
+                  <a href="#" style="color: #55acee;"><i class="fa-brands fa-twitter"></i></a>
+                  <a href="https://www.instagram.com/audry.monica/" style="color: #e6683c;"><i class="fa-brands fa-instagram"></i></a>
+                  <a href="https://www.linkedin.com/in/monica-audry-74504121a/" style="color: #012165;"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+              </div>
+            </div>
+
+            <div class="column">
+              <div class="card2">
+                <div class="img-container">
+                  <img src="../image/member4.jpg">
+                </div>
+                <h3>M Isyraf Naufal Aththabarani</h3>
+                <p>Web Developer</p>
+                <div class="icons">
+                  <a href="#" style="color: #3b5999;"><i class="fa-brands fa-facebook"></i></a>
+                  <a href="#" style="color: #55acee;"><i class="fa-brands fa-twitter"></i></a>
+                  <a href="https://www.instagram.com/isyrafna/" style="color: #e6683c;"><i class="fa-brands fa-instagram"></i></a>
+                  <a href="https://www.linkedin.com/in/muhammad-isyraf-naufal-aththabarani-b583b8215/" style="color: #012165;"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
         `;
   },
   async afterRender() {
