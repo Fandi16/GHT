0ee26e47bc9aec7e9fde1ee6502223e059306d48
ilhamptrr <ilham.ptrr@gmail.com>
2023-06-16 21:01:54 +0700

    update footer

--- src/scripts/components/footer.js
+++ src/scripts/components/footer.js
@@ -13,24 +13,23 @@ class Jumbotran extends HTMLElement {
                     <h3>Link Utama</h3>
                     <a href="#"> <i class="fas fa-angle-right"></i> Home</a>
                     <a href="#/explore"> <i class="fas fa-angle-right"></i> Explore</a>
-                    <a href=""> <i class="fas fa-angle-right"></i> About US</a>
-                    <a href="#/login"> <i class="fas fa-angle-right"></i> Login</a>
+                    <a href="#/about"> <i class="fas fa-angle-right"></i> About US</a>
                 </div>
 
                 <div class="box">
                     <h3>Info Kontak</h3>
                     <a href="#"> <i class="fas fa-phone"></i> +62811112225</a>
                     <a href="#"> <i class="fas fa-phone"></i> +62851111222 </a>
-                    <a href="#"> <i class="fas fa-envelope"></i> monicaaudry@gmail.com </a>
+                    <a href="#"> <i class="fas fa-envelope"></i> greenhuntertour@gmail.com </a>
                     <a href="#"> <i class="fas fa-map"></i> NTT, Indonesia</a>
                 </div>
 
                 <div class="box">
                     <h3>Follow Kami</h3>
-                    <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
-                    <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
-                    <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
-                    <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+                    <a href="https://facebook.com/"> <i class="fab fa-facebook-f"></i> facebook </a>
+                    <a href="https://twitter.com/"> <i class="fab fa-twitter"></i> twitter </a>
+                    <a href="https://www.instagram.com/"> <i class="fab fa-instagram"></i> instagram </a>
+                    <a href="https://www.linkedin.com/"> <i class="fab fa-linkedin"></i> linkedin </a>
                 </div>
 
             </div>
