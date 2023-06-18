7f2aa822b5dcc37328f3cf37a9b7db97a5c9aa67
ilhamptrr <ilham.ptrr@gmail.com>
2023-06-19 00:09:00 +0700

    update

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -3,44 +3,43 @@ import data from './DATA.json';
 const Home = {
   async render() {
     return `
-      <div class="content">
-      <h2>GHT</h2>
-          <div class="information">
-              <div class="content-description">
-                  <h1 class="title">Discover Your Destination</h1>
-                  <p>
-                      Temukan tujuan wisata sesuai keinginanmu untuk menikmati liburan yang luar biasa dan tak
-                      terlupakan.
-                  </p>
-              </div>
-              <div class="content-image">
-                  <img src="../image/destination.jpg" alt="Destination">
-              </div>
+        <div class="content">
+            <div class="information">
+                <div class="content-description">
+                    <h1 class="title">Discover Your Destination</h1>
+                    <p>
+                        Temukan tujuan wisata sesuai keinginanmu untuk menikmati liburan yang luar biasa dan tak
+                        terlupakan.
+                    </p>
+                </div>
+                <div class="content-image">
+                    <img src="../image/destination.jpg" alt="Destination">
+                </div>
 
-              <div class="content-description">
-                  <h1 class="title">Choose Your Perfect Accommodation</h1>
-                  <p>
-                      Pilih penginapan yang nyaman sesuai preferensimu untuk nikmati liburanmu. Tersedia berbagai
-                      pilihan penginapan sesuai kebutuhan dan keinginanmu.
-                  </p>
-              </div>
-              <div class="content-image">
-                  <img src="../image/accommodation.jpg" alt="Destination">
-              </div>
-          </div>
-      </div>
+                <div class="content-description">
+                    <h1 class="title">Choose Your Perfect Accommodation</h1>
+                    <p>
+                        Pilih penginapan yang nyaman sesuai preferensimu untuk nikmati liburanmu. Tersedia berbagai
+                        pilihan penginapan sesuai kebutuhan dan keinginanmu.
+                    </p>
+                </div>
+                <div class="content-image">
+                    <img src="../image/accommodation.jpg" alt="Destination">
+                </div>
+            </div>
+        </div>
 
-      <div class="content-wisata">
-          <div class="content">
-              <h1>TOP Destination</h1>
-          </div>
+        <div class="content-wisata">
+            <div class="content">
+                <h1>TOP Destination</h1>
+            </div>
 
-          <div class="list" id="tes"></div>
- 
-          <div class="content">
-              <button><a href="/#/explore">More Destination</a></button>
-          </div>
-      </div>
+            <div class="list" id="tes"></div>
+    
+            <div class="content">
+                <button><a href="/#/explore">More Destination</a></button>
+            </div>
+        </div>
     `;
   },
 
